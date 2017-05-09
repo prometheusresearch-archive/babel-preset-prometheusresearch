a2d5126f4ad3a08223a44613cb22e5918f5dc005
Andrey Popp <8mayday@gmail.com>
2017-05-09 17:03:59 +0500

    feat: use babel-preset-react-app as a base

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 module.exports = {
   presets: [
-    require('babel-preset-es2015'),
-    require('babel-preset-stage-1'),
-    require('babel-preset-react'),
+    require.resolve('babel-preset-react-app'),
   ],
   plugins: [
-    require('babel-plugin-transform-decorators-legacy').default,
+    // Support decorators syntax for a while
+    require.resolve('babel-plugin-transform-decorators-legacy'),
+    // Support export extensions
+    require.resolve('babel-plugin-transform-export-extensions'),
   ],
-  env: {
-    test: {
-      plugins: [
-        require('babel-plugin-espower'),
-      ]
-    }
-  }
 };
